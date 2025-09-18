481f71c4665acb883853a6c381b364fd510ff0ba
Sneider <sneiderpossoosorio@gmail.com>
2025-09-18 18:25:09 +0000

    Implementar la función mostrarEstudiantes y el menú de opciones

--- Taller#2/Taller.c
+++ Taller#2/Taller.c
@@ -16,3 +16,31 @@ Estudiante estudiantes[NUM_ESTUDIANTES] = {
     {2024105, 3.8},
     {2024106, 4.7}
 };
+// Función para mostrar estudiantes
+void mostrarEstudiantes(const Estudiante arr[], int n) {
+    printf("\nCod\tNota\n");
+    for (int i = 0; i < n; i++)
+        printf("%d\t%.2f\n", arr[i].codigo, arr[i].nota);
+}
+
+// Opciones del menú
+enum OpcionesMenu {
+    NOTA_MAX = 1,
+    PROMEDIO,
+    ORDENAR_COD,
+    MOSTRAR,
+    SALIR
+};
+
+int mostrarMenu() {
+    int op;
+    printf("\n=== MENÚ ===\n");
+    printf("1. Nota máxima (recursivo)\n");
+    printf("2. Promedio (recursivo)\n");
+    printf("3. Ordenar por código (Selection Sort Recursivo)\n");
+    printf("4. Mostrar estudiantes\n");
+    printf("5. Salir\n");
+    printf("Opción: ");
+    scanf("%d", &op);
+    return op;
+}
